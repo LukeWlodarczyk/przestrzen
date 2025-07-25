78ba78e514c4a446d0bc010c26e58297f5b0d5d6
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-07-25 22:58:08 +0200

    chore: disable devToolbar (#26)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     // https://github.com/withastro/astro/issues/14030
     // @ts-expect-error
