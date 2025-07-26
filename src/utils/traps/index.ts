37e87d980f24d65fb0c4d199de8c3207aa5ed806
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-07-26 18:04:36 +0200

    feat: add ariaTrap (#27)

--- src/utils/traps/index.ts
+++ src/utils/traps/index.ts
@@ -1,3 +1,4 @@
 export { createFocusTrap } from "focus-trap";
+export { default as createAriaTrap } from "./ariaTrap";
 export { default as createScrollLock } from "./scrollLock";
 export { default as combineTraps, type Trap } from "./combineTraps";
