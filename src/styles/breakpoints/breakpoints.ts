8e3b653decc5536e9a48c5495f7fd61fe14215be
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-01 16:28:40 +0200

    feat: add custom breakpoints definition (#30)

---
+++ src/styles/breakpoints/breakpoints.ts
@@ -0,0 +1,11 @@
+// Keep in sync with breakpoints.css
+
+const BREAKPOINTS = {
+  sm: "35rem",
+  md: "48rem",
+  lg: "64rem",
+  xl: "80rem",
+  "2xl": "96rem",
+} as const;
+
+export default BREAKPOINTS;
