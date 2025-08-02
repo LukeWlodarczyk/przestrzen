31f690fcb4992b5b3115a5c7cbb5abb05365817d
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-02 21:56:44 +0200

    chore: setup Contentful SDK and basic API configuration (#34)

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,10 @@ interface ImportMetaEnv {
   readonly PUBLIC_SITE_URL: string;
 
   readonly CLOUDINARY_CLOUD_NAME: string;
+
+  readonly CONTENTFUL_SPACE_ID: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
 }
 
 interface ImportMeta {
