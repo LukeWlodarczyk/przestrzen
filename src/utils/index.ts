627a2a12253c8383db67bbb2add217a6b3cb71c0
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-05 10:52:57 +0200

    feat: add swipe util (#46)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,4 +4,5 @@ export { default as getRequiredElements } from "./getRequiredElements.ts";
 export { default as whenHighBandwidth } from "./whenHighBandwidth.ts";
 export { default as hideAndRemove } from "./hideAndRemove.ts";
 export { default as getRequiredCssVariable } from "./getRequiredCssVariable.ts";
+export { default as initSwipe } from "./swipe.ts";
 export { type PropsWithChildren } from "./types.ts";
