39ec0b99d9d5b4442eef5fbaf8b8dc7202482873
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-12 19:54:17 +0200

    feat: add Textarea component (#54)

---
+++ src/components/form/Textarea.tsx
@@ -0,0 +1,77 @@
+import type { FC, TextareaHTMLAttributes } from "react";
+
+import { tw } from "@utils/index.ts";
+
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  id: string;
+  className?: string;
+  label: string;
+  hint: string;
+  error?: string;
+  isValidFilled: boolean;
+}
+
+const baseClasses = tw("relative flex flex-grow flex-col");
+
+const Textarea: FC<Props> = ({
+  className,
+  id,
+  label,
+  placeholder,
+  hint,
+  error,
+  isValidFilled,
+  ...attrs
+}) => {
+  const errorMessageId = `${id}-error-message`;
+  const hintMessageId = `${id}-hint-message`;
+
+  const errorClasses = error
+    ? "border-earth-red text-earth-red placeholder:text-earth-red/50 focus:border-earth-red"
+    : "";
+
+  const validFilledClasses = isValidFilled
+    ? "border-forest-green/50 focus:border-forest-green/50 text-forest-green/90"
+    : "";
+
+  return (
+    <div className={tw(baseClasses, className)}>
+      <label
+        className="relative left-1 mb-(--space-3xs-2xs) block text-sm font-light text-charcoal"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <div className="noise-30 grow">
+        <textarea
+          aria-invalid={error ? "true" : "false"}
+          aria-describedby={`${hintMessageId} ${error ? `${errorMessageId}` : ""}`}
+          className={tw(
+            "block h-full w-full resize-none rounded-xl border border-charcoal/10 bg-sand-beige/40 px-(--space-xs-s) py-(--space-2xs-xs) font-sans text-base font-extralight text-charcoal duration-300 outline-none focus:border-charcoal/40",
+            errorClasses,
+            validFilledClasses,
+          )}
+          id={id}
+          placeholder={placeholder}
+          {...attrs}
+        />
+      </div>
+      <span
+        className={tw(
+          "pointer-events-none absolute right-[8px] bottom-[-25px] text-sm font-light text-earth-red opacity-0 transition-opacity duration-300",
+          error ? "opacity-100" : "",
+        )}
+        role="alert"
+        aria-live="polite"
+        id={errorMessageId}
+      >
+        {error}
+      </span>
+      <span className="sr-only" hidden id={hintMessageId}>
+        {hint}
+      </span>
+    </div>
+  );
+};
+
+export default Textarea;
