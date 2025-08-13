fb17421da3c59ddc9b5fd17927ed4ee9973b761f
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-13 17:38:09 +0200

    refactor: update directory structure for blog feature (#55)

---
+++ src/features/blog/loaders/previewLoader.ts
@@ -0,0 +1,23 @@
+import contentfulClient, { timeToRead } from "@lib/contentful";
+
+import type { BlogPreview } from "@features/blog/types";
+
+const loadPreviewData = async () => {
+  const entries =
+    await contentfulClient.withoutUnresolvableLinks.getEntries<BlogPreview>({
+      content_type: "blogPreview",
+      limit: 1,
+    });
+
+  const [data] = entries.items;
+
+  return {
+    ...data.fields,
+    recomendedBlogPosts: data.fields.recomendedBlogPosts
+      .map((post) => post?.fields)
+      .filter((post) => !!post)
+      .map((post) => ({ ...post, timeToRead: timeToRead(post.body) })),
+  };
+};
+
+export default loadPreviewData;
