dcb7f58c224a3f0d2c79846199b5768ca736837e
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-13 17:58:15 +0200

    refactor: update directory structure for contact feature (#55)

---
+++ src/features/contact/loaders/previewLoader.ts
@@ -0,0 +1,17 @@
+import contentfulClient from "@lib/contentful";
+
+import type { ContactPreview } from "@features/contact/types";
+
+const loadPreviewData = async () => {
+  const entries =
+    await contentfulClient.withoutUnresolvableLinks.getEntries<ContactPreview>({
+      content_type: "contactPreview",
+      limit: 1,
+    });
+
+  const [data] = entries.items;
+
+  return data.fields;
+};
+
+export default loadPreviewData;
