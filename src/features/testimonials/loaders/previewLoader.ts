a3e5ee4dd1f0f481457628df39141c306371d470
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-13 17:46:19 +0200

    refactor: update directory structure for testimonials feature (#55)

---
+++ src/features/testimonials/loaders/previewLoader.ts
@@ -0,0 +1,27 @@
+import contentfulClient from "@lib/contentful";
+
+import type { TestimonialsPreview } from "@features/testimonials/types";
+
+const loadPreviewData = async () => {
+  const entries =
+    await contentfulClient.withoutUnresolvableLinks.getEntries<TestimonialsPreview>(
+      {
+        content_type: "testimonialsPreview",
+        limit: 1,
+      },
+    );
+
+  const [data] = entries.items;
+
+  return {
+    ...data.fields,
+    testimonials: data.fields.testimonials
+      .filter((testimonial) => !!testimonial)
+      .map((testimonial) => ({
+        ...testimonial.fields,
+        id: testimonial.sys.id,
+      })),
+  };
+};
+
+export default loadPreviewData;
