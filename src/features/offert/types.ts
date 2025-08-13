3af9d179a734c6098564ba9d5773991df25ed24c
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-08-13 17:01:29 +0200

    refactor: update directory structure for offert feature (#55)

---
+++ src/features/offert/types.ts
@@ -0,0 +1,52 @@
+import type { EntryFieldTypes } from "contentful";
+
+export interface OffertPreview {
+  contentTypeId: "offertPreview";
+  fields: {
+    heading: EntryFieldTypes.Text;
+    headline: EntryFieldTypes.RichText;
+    areasOfSupportHeading: EntryFieldTypes.Text;
+    areasOfSupportDescription: EntryFieldTypes.RichText;
+    cldVideoPublicId: EntryFieldTypes.Text;
+    formsOfSupportHeading: EntryFieldTypes.Text;
+    formsOfSupportDescription: EntryFieldTypes.RichText;
+  };
+}
+
+export interface FormOfSupport {
+  contentTypeId: "formOfSupport";
+  fields: {
+    name: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    description: EntryFieldTypes.Text;
+    image: EntryFieldTypes.AssetLink;
+  };
+}
+
+export interface FormsOfSupportList {
+  contentTypeId: "formsOfSupportList";
+  fields: {
+    listInOrder: EntryFieldTypes.Array<
+      EntryFieldTypes.EntryLink<FormOfSupport>
+    >;
+  };
+}
+
+export interface AreaOfSupport {
+  contentTypeId: "areaOfSupport";
+  fields: {
+    name: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    description: EntryFieldTypes.Text;
+    image: EntryFieldTypes.AssetLink;
+  };
+}
+
+export interface AreasOfSupportList {
+  contentTypeId: "areasOfSupportList";
+  fields: {
+    listInOrder: EntryFieldTypes.Array<
+      EntryFieldTypes.EntryLink<AreaOfSupport>
+    >;
+  };
+}
