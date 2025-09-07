97a201ffaa2036d1b1a789074f6dd748596e718a
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-09-07 23:21:42 +0200

    feat: add Home layout with meta tags and font preload (#64)

--- src/features/home/types.ts
+++ src/features/home/types.ts
@@ -3,6 +3,8 @@ import type { EntryFieldTypes } from "contentful";
 export interface HomePage {
   contentTypeId: "homePage";
   fields: {
+    metaTitle: EntryFieldTypes.Text;
+    metaDescription: EntryFieldTypes.Text;
     heading: EntryFieldTypes.Text;
     headline: EntryFieldTypes.RichText;
     description: EntryFieldTypes.RichText;
