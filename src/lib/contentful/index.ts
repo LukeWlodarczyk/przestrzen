aec594f58328ad6baf4434b52340d6a7158bae17
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-10-01 00:01:03 +0200

    feat: add extractEntryFields contentful helper (#122)

--- src/lib/contentful/index.ts
+++ src/lib/contentful/index.ts
@@ -2,4 +2,5 @@ export { default } from "./client";
 export { default as resolveAssetUrl } from "./resolveAssetUrl";
 export { default as timeToRead } from "./timeToRead";
 export { default as withCache } from "./withCache";
+export { default as extractEntryFields } from "./extractEntryFields";
 export { type ExtractEntryFields } from "./types";
