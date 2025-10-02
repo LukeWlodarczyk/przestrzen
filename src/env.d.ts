3685862fdcfb805c494939cac4b1388912c41bd3
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-10-02 16:54:26 +0200

    fix: set proper cloudinary env var type (#129)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
   readonly PUBLIC_SITE_URL: string;
 
-  readonly CLOUDINARY_CLOUD_NAME: string;
+  readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
 
   readonly CONTENTFUL_SPACE_ID: string;
   readonly CONTENTFUL_DELIVERY_TOKEN: string;
