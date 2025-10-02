83772dc3e136dfa04b85c5b73a1c1ba0c855a014
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-10-02 15:51:58 +0200

    refactor: update notFound types naming convention (#128)

--- src/features/notFound/loaders/mainLoader.ts
+++ src/features/notFound/loaders/mainLoader.ts
@@ -1,6 +1,6 @@
 import contentfulClient, { withCache } from "@lib/contentful";
 
-import type { NotFoundPage } from "@features/notFound/types";
+import type { NotFoundPageSkeleton } from "@features/notFound/types";
 
 // 🚧 Contentful loader (currently unused).
 // Page content is hardcoded directly in the view for now.
@@ -8,7 +8,7 @@ import type { NotFoundPage } from "@features/notFound/types";
 // if we move to a higher Contentful plan and want to manage this content in the CMS.
 
 const loadMainData = async () => {
-  const entries = await contentfulClient.getEntries<NotFoundPage>({
+  const entries = await contentfulClient.getEntries<NotFoundPageSkeleton>({
     content_type: "notFoundPage",
     limit: 1,
   });
