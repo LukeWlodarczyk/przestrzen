b5d1241d82b642337c5316764358f21cfedd5e43
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2025-10-10 20:26:19 +0200

    fix: rebuild Map styles lazy loading (#144)

--- src/components/interactive/Map/Map.tsx
+++ src/components/interactive/Map/Map.tsx
@@ -4,8 +4,7 @@ import { Map as MapLibre } from "@vis.gl/react-maplibre";
 
 import { tw } from "@utils/index";
 
-import useMapLoadingState from "./hooks/useMapLoadingState";
-import useMapStylesLoadingState from "./hooks/useMapStylesLoadingState";
+import useMap from "./hooks/useMap";
 
 import Placeholder from "./components/Placeholder";
 import Marker from "./components/Marker";
@@ -13,6 +12,7 @@ import Marker from "./components/Marker";
 import {
   DEFAULT_ZOOM,
   MAPLIBRE_CANVAS_CLASS,
+  MAP_LIBRE_REMOTE_STYLE_URL,
   OPEN_FREE_MAP_STYLE_URL,
 } from "./config";
 
@@ -24,34 +24,36 @@ interface MapProps {
 }
 
 const baseClasses = tw(
-  "relative h-full w-full overflow-hidden rounded-2xl border border-warm-yellow outline-offset-2 has-focus-visible:outline-2 has-focus-visible:outline-warm-yellow",
+  "relative h-full w-full overflow-hidden rounded-2xl border border-warm-yellow",
+  "outline-offset-2 has-focus-visible:outline-2 has-focus-visible:outline-warm-yellow",
   `[&_${MAPLIBRE_CANVAS_CLASS}]:outline-none`,
 );
 
 const Map: FC<MapProps> = ({ className, coordinates }) => {
-  const mapLibreLoadingState = useMapLoadingState();
-
-  const stylesheetLoadingState = useMapStylesLoadingState();
-
-  const isFullLoaded =
-    mapLibreLoadingState.isLoaded && stylesheetLoadingState.isLoaded;
+  const { linkRef, onLoad, isLoaded } = useMap();
 
   return (
     <div className={tw(baseClasses, className)}>
+      <link
+        data-href={MAP_LIBRE_REMOTE_STYLE_URL}
+        rel="stylesheet"
+        ref={linkRef}
+      />
+
       <MapLibre
         cooperativeGestures
         mapStyle={OPEN_FREE_MAP_STYLE_URL}
-        onLoad={mapLibreLoadingState.handleOnLoad}
+        onLoad={onLoad}
         initialViewState={{
           longitude: coordinates.lon,
           latitude: coordinates.lat,
           zoom: DEFAULT_ZOOM,
         }}
       >
-        <Marker coordinates={coordinates} isVisible={isFullLoaded} />
+        <Marker coordinates={coordinates} isVisible={isLoaded} />
       </MapLibre>
 
-      <Placeholder isHidden={isFullLoaded} />
+      <Placeholder isHidden={isLoaded} />
     </div>
   );
 };
--- src/components/interactive/Map/hooks/useMapLoadingState.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const useMapLoadingState = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  return { isLoaded, handleOnLoad: () => setIsLoaded(true) };
-};
-
-export default useMapLoadingState;
