dba6d9d91d9d16c7febecce8e7d2e9256a66d3f1
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2026-05-06 16:17:03 +0200

    feat: add map placeholder grid basic animation (#146)

--- src/components/interactive/Map/components/Placeholder.tsx
+++ src/components/interactive/Map/components/Placeholder.tsx
@@ -9,9 +9,15 @@ type Props = {
 const placeholderClasses = {
   base: tw(
     /* z-100000 is applied because .maplibregl-cooperative-gesture-screen has z-index: 99999;  */
-    "noise-15 absolute inset-0 z-100000 rounded-2xl bg-warm-yellow duration-500 ease-out",
+    "noise-15 absolute inset-0 z-100000 grid scale-[1.01] place-items-center rounded-2xl bg-warm-yellow duration-500 ease-out",
   ),
   fadeOut: tw("invisible opacity-0"),
+  grid: tw(
+    "absolute top-0 left-0 h-full w-full",
+    "[background-image:linear-gradient(to_right,rgba(0,0,0,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.08)_1px,transparent_1px)] [background-size:40px_40px]",
+    "[mask-image:radial-gradient(ellipse_at_center,black_25%,transparent_100%)] [-webkit-mask-image:radial-gradient(ellipse_at_center,black_25%,transparent_100%)]",
+    "scale-100 duration-500 ease-out starting:scale-80",
+  ),
 };
 
 const Placeholder: FC<Props> = ({ isHidden }) => (
@@ -21,7 +27,9 @@ const Placeholder: FC<Props> = ({ isHidden }) => (
       placeholderClasses.base,
       isHidden && placeholderClasses.fadeOut,
     )}
-  />
+  >
+    <div className={placeholderClasses.grid}></div>
+  </div>
 );
 
 export default Placeholder;
