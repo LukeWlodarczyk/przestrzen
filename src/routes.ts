e092cfa29f11ba311ec08753100ee77916c09e4f
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2026-05-24 23:54:34 +0200

    refactor: enhance routes code clarity (#158)

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,42 @@
-export const IDS = {
+export const SEGMENTS = {
+  aboutMe: "o-mnie",
+  offer: "oferta",
   formOfSupport: "formy-wsparcia",
   areaOfSupport: "obszary-wsparcia",
+  blog: "blog",
+  contact: "kontakt",
+  reservation: "rezerwacja",
+  office: "gabinet",
+  success: "sukces",
+  notFound: "404",
 } as const;
 
 export const STATIC_ROUTES = {
   home: "/",
-  aboutMe: "/o-mnie/",
-  offert: "/oferta/",
-  formOfSupport: `/oferta/#${IDS.formOfSupport}`,
-  areaOfSupport: `/oferta/#${IDS.areaOfSupport}`,
-  blog: "/blog/",
-  contact: "/kontakt/",
-  contactSuccess: "/kontakt/sukces/",
-  reservation: "/rezerwacja/",
-  reservationSuccess: "/rezerwacja/sukces/",
-  notFound: "/404/",
+  aboutMe: `/${SEGMENTS.aboutMe}/`,
+  offert: `/${SEGMENTS.offer}/`,
+  formOfSupport: `/${SEGMENTS.offer}/#${SEGMENTS.formOfSupport}`,
+  areaOfSupport: `/${SEGMENTS.offer}/#${SEGMENTS.areaOfSupport}`,
+  blog: `/${SEGMENTS.blog}/`,
+  contact: `/${SEGMENTS.contact}/`,
+  contactSuccess: `/${SEGMENTS.contact}/${SEGMENTS.success}/`,
+  reservation: `/${SEGMENTS.reservation}/`,
+  reservationSuccess: `/${SEGMENTS.reservation}/${SEGMENTS.success}/`,
+  notFound: `/${SEGMENTS.notFound}/`,
 } as const;
 
 export const DYNAMIC_ROUTES = {
   formOfSupport: (slug: string) =>
-    `${STATIC_ROUTES.offert}${IDS.formOfSupport}/${slug}/`,
+    `/${SEGMENTS.offer}/${SEGMENTS.formOfSupport}/${slug}/` as const,
   areaOfSupport: (slug: string) =>
-    `${STATIC_ROUTES.offert}${IDS.areaOfSupport}/${slug}/`,
-  office: (slug: string) => `${STATIC_ROUTES.reservation}gabinet/${slug}/`,
-  blogPost: (slug: string) => `${STATIC_ROUTES.blog}${slug}/`,
+    `/${SEGMENTS.offer}/${SEGMENTS.areaOfSupport}/${slug}/` as const,
+  office: (slug: string) =>
+    `/${SEGMENTS.reservation}/${SEGMENTS.office}/${slug}/` as const,
+  blogPost: (slug: string) => `/${SEGMENTS.blog}/${slug}/` as const,
 } as const;
 
 export type StaticRouteKey = keyof typeof STATIC_ROUTES;
-export type StaticRouteValue =
-  (typeof STATIC_ROUTES)[keyof typeof STATIC_ROUTES];
+export type StaticRouteValue = (typeof STATIC_ROUTES)[StaticRouteKey];
 
 export interface StaticRouteLink {
   label: string;
